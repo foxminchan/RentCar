746ad640b060704f5fd3c0b51c59b5795f6f58b1
Nguyen Xuan Nhan <nguyenxuannhan407@gmail.com>
2024-01-19 00:43:42 +0700

    test: add test

---
+++ test/RentCar.Api.Test/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace RentCar.Api.Test;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
---
+++ test/RentCar.Website.Test/BunitTestContext.cs
@@ -0,0 +1,17 @@
+using Bunit;
+using NUnit.Framework;
+
+namespace RentCar.Website.Test;
+
+/// <summary>
+/// Test context wrapper for bUnit.
+/// Read more about using <see cref="BunitTestContext"/> <seealso href="https://bunit.dev/docs/getting-started/writing-tests.html#remove-boilerplate-code-from-tests">here</seealso>.
+/// </summary>
+public abstract class BunitTestContext : TestContextWrapper
+{
+	[SetUp]
+	public void Setup() => TestContext = new Bunit.TestContext();
+
+	[TearDown]
+	public void TearDown() => TestContext?.Dispose();
+}
