fff184a075f34a9fbfd49d9a65354269ad43d003
Nguyen Xuan Nhan <nguyenxuannhan407@gmail.com>
2024-01-24 18:19:05 +0700

    feat: add photo retry

--- src/RentCar.Infrastructure/Cloudinary/CloudinaryService.cs
+++ src/RentCar.Infrastructure/Cloudinary/CloudinaryService.cs
@@ -5,11 +5,16 @@ using Ardalis.Result;
 using CloudinaryDotNet;
 using CloudinaryDotNet.Actions;
 using Microsoft.AspNetCore.Http;
+using Polly;
+using Polly.Retry;
 
 namespace RentCar.Infrastructure.Cloudinary;
 
 public sealed class CloudinaryService(ICloudinaryUploadApi cloudinary) : ICloudinaryService
 {
+    private readonly AsyncRetryPolicy _retryPolicy = Policy.Handle<Exception>()
+            .WaitAndRetryAsync(3, retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt)));
+
     public async Task<Result<CloudinaryResult>> AddPhotoAsync(IFormFile? file)
     {
         if (file is not { Length: > 0 })
@@ -23,7 +28,7 @@ public sealed class CloudinaryService(ICloudinaryUploadApi cloudinary) : ICloudi
             Transformation = new Transformation().Height(700).Width(700).Crop("fill").Gravity("face")
         };
 
-        var uploadResult = await cloudinary.UploadAsync(uploadParams);
+        var uploadResult = await _retryPolicy.ExecuteAsync(async () => await cloudinary.UploadAsync(uploadParams));
 
         return uploadResult.Error is { }
             ? Result<CloudinaryResult>.Invalid(new ValidationError(uploadResult.Error.Message))
@@ -32,7 +37,7 @@ public sealed class CloudinaryService(ICloudinaryUploadApi cloudinary) : ICloudi
 
     public async Task<Result<string>> DeletePhotoAsync(string publicId)
     {
-        var result = await cloudinary.DestroyAsync(new DeletionParams(publicId));
+        var result = await _retryPolicy.ExecuteAsync(async () => await cloudinary.DestroyAsync(new(publicId)));
         return result.Error is { }
             ? Result<string>.Error(result.Error.Message)
             : Result<string>.Success(result.Result);
